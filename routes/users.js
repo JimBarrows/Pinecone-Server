313d3266e2327fdee3cf8626bbb9c54d921a449d
Jim Barrows <jim@reallybigtree.com>
2016-07-20 16:02:30 -0700

    Added ability to update the account with the facebook id.

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,39 @@
-var express  = require('express');
-var passport = require('passport');
-var Account  = require('pinecone-models/src/Account');
-var router   = express.Router();
+"use strict";
+var express           = require('express');
+var passport          = require('passport');
+var Account           = require('pinecone-models/src/Account');
+var router            = express.Router();
+const isAuthenticated = require('../authentication');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
 	res.send('respond with a resource. Foo.');
 });
 
+router.put('/facebookId', isAuthenticated, function (req, res) {
+	let owner = req.user.id;
+	let facebookUserId = req.body.facebookUserId;
+	Account.findOneAndUpdate({_id: owner}, {
+				facebookUserId
+			})
+			.exec()
+			.then((updatedAccount) => {
+				console.log("updated: ", updatedAccount);
+				res.status(200).end()
+			})
+			.catch((error) => {
+				console.log("Error updating account ", owner, " with error ", error);
+				res.status(400).end();
+			});
+});
+
 router.post('/register', function (req, res) {
-	Account.register(new Account({username: req.body.username}), req.body.password, function (err, account) {
+	Account.register(new Account({
+		username: req.body.username,
+		facebookUserId: ''
+	}), req.body.password, function (err, account) {
 		if (err) {
-			console.log("While registering, could not registeruser because ", err);
+			console.log("While registering, could not register user because ", err);
 			return res.json({error: err.message});
 		}
 		passport.authenticate('local')(req, res, function () {
@@ -30,6 +52,7 @@ router.post('/login', passport.authenticate('local'), function (req, res) {
 	res.json({
 		username: req.user.username
 		, id: req.user._id
+		, facebookUserId: req.user.facebookUserId
 	})
 });
 
