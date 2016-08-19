4147f1cc32a18ff968b14d67268abd5c6c1563d2
Jim Barrows <jim@reallybigtree.com>
2016-08-18 22:20:02 -0700

    Put updates a campaign correctly.

--- routes/campaign.js
+++ routes/campaign.js
@@ -26,4 +26,18 @@ router.get('/:campaignId', isAuthenticated, function (req, res) {
 			})
 });
 
+router.put('/:campaignId', isAuthenticated, function (req, res) {
+	const campaignId = req.params.campaignId;
+	if (req.body.owner == req.user._id) {
+		Campaign.findOneAndUpdate({_id: campaignId, owner: req.user._id}, req.body)
+				.then((original) => original ? res.status(204).end() : res.status(404).end())
+				.catch((error) => {
+					console.log("Could not find one and update for id ", campaignId, " and body of ", req.body, " because ", error);
+					res.status(400).end();
+				});
+	} else {
+		res.status(400).end();
+	}
+
+});
 module.exports = router;
--- test/campaign.spec.js
+++ test/campaign.spec.js
@@ -77,6 +77,74 @@ describe("/campaign/:campaignId", function () {
 					.catch((error) => {
 						done(error);
 					});
+		});
+	});
+
+	describe('put method', function () {
+		var campaign = {};
+
+		beforeEach(function (done) {
+			Campaign.create({
+						name: 'Test Campaign 1',
+						owner: loggedInUser._id
+					})
+					.then(function (newCampaign) {
+						campaign = newCampaign;
+						done();
+					})
+					.catch(function (error) {
+						console.log("/api/campaign/:campaignId get method beforeAll error: ", error);
+						done(error);
+					})
+		});
+
+		it("must update the campaign", function (done) {
+			let updatedCampaign = {
+				name: 'Test Campaign 1 updated',
+				owner: loggedInUser._id,
+				_id: campaign._id
+			};
+			client.put('/campaign/' + campaign._id, updatedCampaign)
+					.then((response) => {
+						expect(response.status).to.be.equal(204);
+						Campaign.findById(campaign._id)
+								.then((found) => expect(found.name).to.be.equal(updatedCampaign.name))
+								.catch((error) => done(error));
+						done();
+					})
+					.catch((error) => done(error));
+		});
+
+		it("must return a status of 400 if the campaign being updated is not owned by the logged in user.", function (done) {
+			Account.create({username: user.username + "2", hash, salt})
+					.then((newAccount) => Campaign.create({name: 'Test Campaign 2', owner: newAccount._id}))
+					.then((unownedCampaign) => client.put('/campaign/' + unownedCampaign._id, {
+						name: 'Test Campaign 2 updated',
+						owner: unownedCampaign.owner,
+						_id: unownedCampaign._id
+					}))
+					.then((response) => {
+						expect(response.status).to.be.equal(400);
+						done();
+					})
+					.catch((error) => {
+						console.log("error: ", error);
+						done(error);
+					});
+		});
+
+		it("must not override the owner.", function (done) {
+			Account.create({username: user.username + "2", hash, salt})
+					.then((newAccount) => client.put('/campaign/' + campaign._id, {
+						name: campaign.name,
+						owner: newAccount.owner,
+						_id: campaign._id
+					}))
+					.then((response) => {
+						expect(response.status).to.be.equal(400);
+						done();
+					})
+					.catch((error) => done(error));
 		})
-	})
+	});
 });
