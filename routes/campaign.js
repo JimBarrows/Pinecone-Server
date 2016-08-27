1f176a492e5c169f367986a926fe2b0f78d7b5fd
Jim Barrows <jim@reallybigtree.com>
2016-08-27 08:14:35 -0700

    Can delete a message.

--- routes/campaign.js
+++ routes/campaign.js
@@ -8,6 +8,16 @@ import isAuthenticated from "../authentication";
 
 const router = express.Router();
 
+router.delete("/:campaignId", isAuthenticated, function (req, res) {
+	let {campaignId} = req.params;
+	Campaign.remove({_id: campaignId})
+			.then(() => res.status(200).end())
+			.catch((error) => {
+				console.log("Couldn't delete campaign ", contentId, " becuase ", error);
+				res.status(400).end();
+			})
+});
+
 router.get('/:campaignId', isAuthenticated, function (req, res) {
 	const campaignId = req.params.campaignId;
 	Campaign.findOne({_id: campaignId, owner: req.user._id})
@@ -29,8 +39,8 @@ router.get('/:campaignId', isAuthenticated, function (req, res) {
 router.put('/:campaignId', isAuthenticated, function (req, res) {
 	const campaignId = req.params.campaignId;
 	if (req.body.owner == req.user._id) {
-		Campaign.findOneAndUpdate({_id: campaignId, owner: req.user._id}, req.body)
-				.then((original) => original ? res.status(204).end() : res.status(404).end())
+		Campaign.findOneAndUpdate({_id: campaignId, owner: req.user._id}, req.body, {new: true})
+				.then((updated) => updated ? res.json(updated).status(200).end() : res.status(404).end())
 				.catch((error) => {
 					console.log("Could not find one and update for id ", campaignId, " and body of ", req.body, " because ", error);
 					res.status(400).end();
